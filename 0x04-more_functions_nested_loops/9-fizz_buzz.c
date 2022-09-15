ac2f4cb810676f6082b0837977e70b639d1c6121
Everlyn-Kihiu <kihiungendo@gmail.com>
2022-09-15 14:51:03 -0700

    init

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -9,7 +8,7 @@ int main(void)
 {
 int n;
 n = 1;
-printf("%d", n);
+printf("%d ", n);
 for (n = 2; n <= 100; n++)
 {
 if ((n % 3 == 0) && (n % 5 == 0))
